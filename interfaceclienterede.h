5aa02a441aa936a1be8c50e2987f0626aa249158
dizus <dizus@tequilamovel.boga>
2009-07-17 01:30:09 -0300

    interface rede cliente

---
+++ interfaceclienterede.h
@@ -0,0 +1,48 @@
+#ifndef INTERFACECLIENTEREDE_H
+#define INTERFACECLIENTEREDE_H
+
+#include <QObject>
+
+class InterfaceClienteRede : public QObject
+{
+    Q_OBJECT
+
+    signals:
+        void
+        EnviaPecas(qint16);
+
+        void
+        EnviaGameOver();
+
+        void
+        EnviaChat(QString);
+
+        void
+        EnviaEncaixe();
+
+        void
+        EnviaTecla(qint16);
+
+        void
+        EnviaStart();
+
+        void
+        RecebePecas(qint16);
+
+        void
+        RecebeGameOver();
+
+        void
+        RecebeChat(QString);
+
+        void
+        RecebeEncaixe();
+
+        void
+        RecebeTecla(qint16);
+
+        void
+        RecebeStart();
+};
+
+#endif // INTERFACECLIENTEREDE_H
