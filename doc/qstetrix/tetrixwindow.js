bafba24ee4993fec04dae7a5129e20b2c00f4522
Lucius <vibortolotti@gmail.com>
2009-07-21 13:49:19 -0300

    Insercao do projeto exemplo

---
+++ doc/qstetrix/tetrixwindow.js
@@ -0,0 +1,15 @@
+function TetrixWindow(ui)
+{
+    this.ui = ui;
+
+    var boardUi = ui.findChild("board");
+    boardUi.nextPieceLabel = ui.findChild("nextPieceLabel");
+
+    this.board = new TetrixBoard(boardUi);
+
+    ui.findChild("startButton").clicked.connect(this.board, this.board.start);
+    ui.findChild("quitButton").clicked.connect(Qt.App.quit);
+    boardUi.scoreChanged.connect(ui.findChild("scoreLcd")["display(int)"]);
+    boardUi.levelChanged.connect(ui.findChild("levelLcd")["display(int)"]);
+    boardUi.linesRemovedChanged.connect(ui.findChild("linesLcd")["display(int)"]);
+}
